ad3a6c8337780baf3bdaccedd1c7cc8488410edc
Sergio Dominguez <sergiofri88@gmail.com>
2022-10-10 13:15:53 +0200

    Agregar archivos de proyecto.

---
+++ CalcularAreacirculo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularAreacirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("¡Hola mundo!");
+
+            ///int edad = 25;
+            //int alumnos = 24;
+            //string curso = "DEINT";
+
+            //Console.WriteLine("Tu edad es de " + edad + " años");
+            //Console. WriteLine ($"Ти edad es de {edad} años");
+            // Console. WriteLine ("Tu edad es de {0}", edad); 
+            //Console.WriteLine("En la clase de {0} hay {1} alumnos", curso, alumnos);
+
+            Console.WriteLine("Introduce un valor para el radio");
+            int radio = int.Parse(Console.ReadLine());
+            const double PI = 3.14;
+
+            double area = PI * Math.Pow(radio, 2);
+
+            Console.WriteLine($"Tu area del circulo de radio {radio} es {area}");
+
+
+
+        }
+    }
+}
